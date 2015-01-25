5bda5c16fcf7a99b8d33fe40101edb66684870f9
bttf <adnanchowdhury88@gmail.com>
2015-01-25 14:21:23 -0500

    fixed login issues

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,12 +1,38 @@
 import Ember from 'ember';
 
+function addUserIfDoesNotExist(_this, uid) {
+  _this.get('store').find('user', uid).then(function(user) {
+    if (user) {
+      console.log('user exists');
+      _this.transitionToRoute('index');
+    }
+  }, function(err) {
+    if (err.message.indexOf('no record was found') !== -1) {
+      console.log('user does not exist');
+      var user = _this.get('store').createRecord('user', {
+        uid: uid
+      });
+      user.set('id', uid);
+      user.save().then(function(user) {
+        console.log('user created', user);
+        _this.transitionToRoute('index');
+      }, function(err) {
+        console.log('error creating user', err);
+      });
+    } else {
+      console.log('error', err);
+    }
+  });
+}
+
 export default Ember.Controller.extend({
   actions: {
     fbLogin: function() {
       var _this = this;
       this.get('session').authenticate('authenticator:facebook').then(function() {
         console.log('login success');
-        _this.transitionToRoute('index');
+        var uid = _this.get('session.authData.uid');
+        addUserIfDoesNotExist(_this, uid);
       }, function(err) {
         console.log('login failure', err);
       });
@@ -15,8 +41,8 @@ export default Ember.Controller.extend({
     gitLogin: function() {
       var _this = this;
       this.get('session').authenticate('authenticator:github').then(function() {
-        console.log('login success');
-        _this.transitionToRoute('index');
+        var uid = _this.get('session.authData.uid');
+        addUserIfDoesNotExist(_this, uid);
       }, function(err) {
         console.log('login failure', err);
       });
@@ -26,7 +52,8 @@ export default Ember.Controller.extend({
       var _this = this;
       this.get('session').authenticate('authenticator:google').then(function() {
         console.log('login success');
-        _this.transitionToRoute('index');
+        var uid = _this.get('session.authData.uid');
+        addUserIfDoesNotExist(_this, uid);
       }, function(err) {
         console.log('login failure', err);
       });
@@ -36,7 +63,8 @@ export default Ember.Controller.extend({
       var _this = this;
       this.get('session').authenticate('authenticator:anonymous').then(function() {
         console.log('login success');
-        _this.transitionToRoute('index');
+        var uid = _this.get('session.authData.uid');
+        addUserIfDoesNotExist(_this, uid);
       }, function(err) {
         console.log('login failure', err);
       });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,36 +1,5 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-function addUserIfDoesNotExist(uid) {
-  var _this = this;
-  this.get('store').find('user', uid).then(function(user) {
-    if (user) {
-      console.log('user exists');
-    }
-  }, function(err) {
-    if (err.message.indexOf('no record was found') !== -1) {
-      console.log('user does not exist');
-      var user = _this.get('store').createRecord('user', {
-        uid: uid
-      });
-      user.set('id', uid);
-      user.save().then(function(user) {
-        console.log('user created', user);
-      }, function(err) {
-        console.log('error creating user', err);
-      });
-    } else {
-      console.log('error', err);
-    }
-  });
-}
-
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  actions: {
-    sessionAuthenticationSucceeded: function() {
-      var _this = this;
-      var uid = this.get('session.authData.uid');
-      addUserIfDoesNotExist.call(this, uid);
-    }
-  }
 });
