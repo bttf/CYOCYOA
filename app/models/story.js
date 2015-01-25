22af8675820049e996099fc098b7d513288c05f7
bttf <adnanchowdhury88@gmail.com>
2015-01-25 16:08:45 -0500

    first page business sorted out

--- app/models/story.js
+++ app/models/story.js
@@ -7,8 +7,11 @@ var attr = DS.attr,
 export default DS.Model.extend({
   user: belongsTo('user', { async: true, inverse: 'stories' }),
   title: attr('string'),
-  firstPage: belongsTo('page'),
   pages: hasMany('page', { async: true, inverse: 'story' }),
   img: attr('string'),
-  publish: belongsTo('publish', { async: true })
+  publish: belongsTo('publish', { async: true }),
+  firstPage: function() {
+    var pages = this.get('pages');
+    return pages.objectAt(0);
+  }.property('pages')
 });
